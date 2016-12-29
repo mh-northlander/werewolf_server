287f2016e1fb5868985ca6aafe4206abb26f5ad2
mh <mh.northlander@gmail.com>
2016-12-30 02:01:47 +0900

    model/role consts

--- model/index.js
+++ model/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     user: require('./user'),
     room: require('./room'),
+    role: require('./role'),
 };
---
+++ model/role.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // role
+    roleVillager : "villager",
+    roleWerewolf : "werewolf",
+    roleSeer     : "seer",
+    roleMedium   : "medium",
+    roleGuard    : "guard",
+
+    // team
+    teamHuman : "human",
+    teamWerewolf : "werewolf",
+    teamOther : "other",
+
+
+    // fromSeer
+    fromSeerHuman       : "human",
+    fromSeerWereeolfolf : "werewolf",
+    fromSeerOther       : "other",    // for future impliment
+
+    // fromMedium
+    fromMediumHuman       : "human",
+    fromMediumWereeolfolf : "werewolf",
+    fromMediumOther       : "other",    // for future impliment
+}
