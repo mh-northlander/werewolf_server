b24021c9f341e7df5ac75e323c1e5e2d4df9e840
Daiki Ihara <ihara@wacul.co.jp>
2017-04-10 19:42:11 +0900

    implement validation for afternoon

--- api/afternoon.js
+++ api/afternoon.js
@@ -11,30 +11,32 @@ module.exports = {
 // imports
 const GamePhaseAfternoon = require('../village/phase').GamePhase.AFTERNOON;
 const evening = require('./evening');
+const common = require("./common")
 
 
 // vate
 function vote(io, socket, village){
     return function(vote){
-        if(phaseCheck(io, socket, village, "vote")){
-            const userId = village.socketIdToUserId(socket.id);
+        if(common.IsValidPhase(io, socket, village, GamePhaseAfternoon, "vote") !== true){
+            return
+        }
+        const userId = village.socketIdToUserId(socket.id);
 
-            // vote: [userId]
-            console.log(village.users.get(userId).name +
-                        " votes to " + village.users.get(vote[0]).name);
+        // vote: [userId]
+        console.log(village.users.get(userId).name +
+                    " votes to " + village.users.get(vote[0]).name);
 
-            // log
-            village.log.day[village.phase.dayCount].vote[userId] = vote;
+        // log
+        village.log.day[village.phase.dayCount].vote[userId] = vote;
 
 
-            if(!village.users.get(userId).readyToShift){ // prevent multi-vote
-                village.addVote(userId, vote);
-                village.users.get(userId).readyToShift = true;
-            }
+        if(!village.users.get(userId).readyToShift){ // prevent multi-vote
+            village.addVote(userId, vote);
+            village.users.get(userId).readyToShift = true;
+        }
 
-            if(village.readyToShift()){
-                end(io, village);
-            }
+        if(village.readyToShift()){
+            end(io, village);
         }
     };
 };
@@ -62,15 +64,3 @@ function end(io, village){
     console.log("afternoon end");
     evening.Begin(io, village);
 };
-
-// validation
-function phaseCheck(io, socket, village, eventName){
-    if(village.phase.gamePhase === GamePhaseAfternoon){
-        return true
-    } else {
-        console.log("badRequest:", eventName, "can't call at", village.phase.gamePhase, "by", village.socketIdToUserId(socket.id));
-        // TODO:before_gameだとjoinRoomに対してはユーザーの特定がIDだとできないのでundefinedになる
-        io.to(socket.id).emit("error", {statusCode:400, message:"badRequest: "+eventName+" can't call at "+ village.phase.gamePhase})
-        return false
-    }
-}
