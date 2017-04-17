aaac6c5857037fa3a68232f222bbc2bccdc101dc
Daiki Ihara <ihara@wacul.co.jp>
2017-04-17 22:22:03 +0900

    [WIP] testing test

--- __test__/server.spec.js
+++ __test__/server.spec.js
@@ -1,16 +1,28 @@
+var should = require('should');
 var io = require('socket.io-client');
 
-var socketURL = 'http://0.0.0.0:5000';
+var socketURL = 'http://localhost:3000/';
 
 var options ={
   transports: ['websocket'],
   'force new connection': true
 };
 
-var chatUser1 = {'name':'Tom'};
-var chatUser2 = {'name':'Sally'};
-var chatUser3 = {'name':'Dana'};
+var chatUser1 = {'name':'Tom', userId:'hogehoge'};
+var chatUser2 = {'name':'Sally', userId:'hogehoge2'};
+var chatUser3 = {'name':'Dana', userId:'hogehoge3'};
 
 describe("Chat Server",function(){
+    var client1 = io.connect(socketURL, options);
+    var client2 = io.connect(socketURL, options);
 
+    it('Should memberChanged function', function(done){
+        client1.emit('joinRoom', chatUser1);
+
+        client2.on('memberChanged', function(data){
+            data[0].should.have.enumerable('name', 'Tom')
+            data[0].should.have.enumerable('id', 'hogehoge')
+            done();
+        });
+    });
 });
--- __test__/test.js
+++ __test__/test.js
@@ -4,7 +4,7 @@ var myModule = require('./myModule');
 describe('myModule', function () {
     describe('greet', function () {
         it('引数に応じて決まった文字列を返すこと', function () {
-            assert.equal(myModule.('taro'), 'Hello,taroo');
+            assert.equal(myModule.greet('taro'), 'Hello,taroo');
         });
     });
 });
