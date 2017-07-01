ac3db84010197f06aa7753b429cba79f0b29b6ff
Daiki Ihara <ihara@wacul.co.jp>
2017-07-02 00:44:25 +0900

    implement test of getCandidatesMap

--- __test__/village/village.spec.js
+++ __test__/village/village.spec.js
@@ -2,13 +2,17 @@ var assert = require('assert');
 
 const village = require('../../village/');
 const phase = require('../../village/phase').GamePhase
+const role = require('../../role')
+
 const vil = village.Village('111111');
 
-var participantsList = {
+const participantsList = {
     'hoge': {userId:'hoge', socktId:'12345', name:'hoge'},
     'piyo': {userId:'piyo', socktId:'23456', name:'piyo'},
     'fuga': {userId:'fuga', socktId:'34567', name:'fuga'}
 }
+
+const roleList = ['Werewolf', 'Villager', 'Seer']
 
 
 describe('villageを動かすテスト', function(){
@@ -33,13 +37,32 @@ describe('villageを動かすテスト', function(){
     });
 
     it('shiftPhase', function(){
+        assert.equal(vil.shiftPhase(phase.BEFOREGAME).gamePhase, phase.BEFOREGAME);
         assert.equal(vil.shiftPhase(phase.NIGHT).gamePhase, phase.NIGHT);
         assert.equal(vil.shiftPhase(phase.MORNING).gamePhase, phase.MORNING);
         assert.equal(vil.shiftPhase(phase.DAYTIME).gamePhase, phase.DAYTIME);
         assert.equal(vil.shiftPhase(phase.AFTERNOON).gamePhase, phase.AFTERNOON);
         assert.equal(vil.shiftPhase(phase.EVENING).gamePhase, phase.EVENING);
         assert.equal(vil.shiftPhase(phase.AFTERGAME).gamePhase, phase.AFTERGAME);
-        assert.equal(vil.shiftPhase(phase.BEFOREGAME).gamePhase, phase.BEFOREGAME);
+        vil.shiftPhase(phase.BEFOREGAME);
+        vil.phase.dayCount = 0;
+    });
+
+    it('action', function(){
+        // TODO
+    });
+
+    it('getCandidatesMap', function(){
+        vil.shiftPhase(phase.NIGHT);
+        let idx = 0;
+        for (const [userId, user] of vil.users){
+            user.role = role[roleList[idx]]();
+            user.role.mountEvents(vil);
+            idx++;
+        }
+        assert.equal(vil.getCandidatesMap().size, 0);
+        vil.phase.dayCount = 1;
+        assert.deepEqual(vil.getCandidatesMap().get('hoge'), ['piyo','fuga']);
     });
 
     it('removeUser', function(){
